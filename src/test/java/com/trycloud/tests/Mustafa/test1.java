e06d1155b1b73f1b1909637c6a815706a9b05948
Mustafa Tanis <mustafatanis97@gmail.com>
2021-01-30 12:52:18 -0500

    added main

--- src/test/java/com/trycloud/tests/Mustafa/test1.java
+++ src/test/java/com/trycloud/tests/Mustafa/test1.java
@@ -3,6 +3,6 @@ package com.trycloud.tests.Mustafa;
 public class test1 {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("testing 1 2 3 4 5");
     }
 }
