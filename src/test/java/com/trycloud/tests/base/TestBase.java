ff713771a6754f496ddd38e6f71a571656cf6b35
Mustafa Tanis <mustafatanis97@gmail.com>
2021-01-30 11:36:10 -0500

    added utilities classes and TestBase

---
+++ src/test/java/com/trycloud/tests/base/TestBase.java
@@ -0,0 +1,29 @@
+package com.trycloud.tests.base;
+
+import com.trycloud.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() throws InterruptedException {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void tearDownMethod(){
+        driver.close();
+    }
+
+
+}
