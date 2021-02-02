5f929d70cdb4f64cf8fed131cd841ef039339d7d
Sevara Isroilova <sevara.i.d@gmail.com>
2021-02-01 20:03:39 -0500

    some changes

--- src/test/java/com/trycloud/tests/Mustafa/test1.java
+++ src/test/java/com/trycloud/tests/Mustafa/test1.java
@@ -1,4 +1,7 @@
 package com.trycloud.tests.Mustafa;
 
 public class test1 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/com/trycloud/tests/Sevara/test1.java
+++ src/test/java/com/trycloud/tests/Sevara/test1.java
@@ -1,17 +1,9 @@
 package com.trycloud.tests.Sevara;
 
-import com.trycloud.Base.TestBase;
-import com.trycloud.utilities.WebOrderUtil;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.Test;
 
-public class  test1 extends TestBase {
-    @Test
-    public void test (){
-    loginToTryCloud(driver);
+public class  test1 {
+
+    public static void main(String[] args) {
+        System.out.println();
     }
-
-    private void loginToTryCloud(WebDriver driver) {
-    }
-
 }
