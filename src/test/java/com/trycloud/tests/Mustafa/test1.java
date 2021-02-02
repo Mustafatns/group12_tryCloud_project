c6d9b2d5de3a388ef690f3910ca51147d4831592
Sevara Isroilova <sevara.i.d@gmail.com>
2021-02-01 20:51:48 -0500

    Created User Story package and TC1 class

--- src/test/java/com/trycloud/tests/Mustafa/test1.java
+++
@@ -1,7 +0,0 @@
-package com.trycloud.tests.Mustafa;
-
-public class test1 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/test/java/com/trycloud/tests/Sevara/test1.java
+++
@@ -1,9 +0,0 @@
-package com.trycloud.tests.Sevara;
-
-
-public class  test1 {
-
-    public static void main(String[] args) {
-        System.out.println("test 1 ");
-    }
-}
