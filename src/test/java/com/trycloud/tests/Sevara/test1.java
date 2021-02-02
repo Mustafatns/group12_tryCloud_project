7d36c39387dea0a49b88bb82ef6ba2dc0a365c8a
Sevara Isroilova <sevara.i.d@gmail.com>
2021-02-01 20:18:57 -0500

    test 1

--- src/test/java/com/trycloud/tests/Sevara/test1.java
+++ src/test/java/com/trycloud/tests/Sevara/test1.java
@@ -4,6 +4,6 @@ package com.trycloud.tests.Sevara;
 public class  test1 {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("test 1 ");
     }
 }
