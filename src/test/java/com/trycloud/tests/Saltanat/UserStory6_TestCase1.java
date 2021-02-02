0156af52c7b575f73e14c84bcf17a6a0b2b52ad1
Saltanat2020 <saabdubek@gmail.com>
2021-02-01 20:06:39 -0500

    deleted

--- src/test/java/com/trycloud/tests/Saltanat/UserStory6_TestCase1.java
+++ src/test/java/com/trycloud/tests/Saltanat/UserStory6_TestCase1.java
@@ -1,15 +1,16 @@
 package com.trycloud.tests.Saltanat;
 
-import com.trycloud.Base.TestBase;
-import org.testng.annotations.Test;
-import com.trycloud.utilities.WebOrderUtil;
 
-public class UserStory6_TestCase1 extends TestBase {
+import com.trycloud.Base.US_1_TestCase_1.LoginToTryCloud;
+import org.testng.annotations.Test;
+
+
+public class UserStory6_TestCase1 extends LoginToTryCloud {
    @Test
    public void verify_users_can_access_to_Talks_module(){
       // 1.Login as a user
-       WebOrderUtil.loginToTryCloud(driver);
 
+ //deleted
        // 2.Click “Notes” module
 
        // 3.Verify the page tile/URL is Notes module’s tile
